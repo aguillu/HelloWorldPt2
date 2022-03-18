cf2eec934a55a7a8f4426d8a0c63531aa0e3e8bc
Alyssa Guillu <aguillu@uchicago.edu>
2022-03-18 10:54:39 -0500

    update

--- scripts.js
+++ scripts.js
@@ -67,3 +67,133 @@ function arrayexample() {
     alert.innerText = names;   
     }
 
+
+
+    var itemTitle = {itemName: String(document.getElementById("itemName").value)};
+    var myLatLng = {lat: Number(document.getElementById("lat").value), lng: Number(document.getElementById("lng").value)};
+    console.log(myLatLng);
+    var map = L.map('map', {
+      center: [myLatLng.lat, myLatLng.lng],
+      zoom: 11
+    });
+    L.esri.basemapLayer('Topographic').addTo(map);
+    L.marker([myLatLng.lat, myLatLng.lng]).addTo(map);
+    // map.invalidateSize(true);
+    setTimeout(function(){map.invalidateSize(true);},500);
+
+    function exportTableToCSV(filename) {
+        //Create a CSV file to download the set.
+        var csv = [];
+        var headings = document.querySelectorAll("thead tr");
+        var rows = document.querySelectorAll("tbody tr");
+      
+        //Create the header row
+        var row = [], cols = headings[0].querySelectorAll("th");
+        for (var m = 0; m < cols.length; m++)
+        row.push('"' + cols[m].innerText + '"');
+        csv.push(row.join(","));
+      
+        //Create the data rows
+        for (var i = 0; i < rows.length; i++) {
+          var row = [], cols = rows[i].querySelectorAll("td");
+          for (var j = 0; j < cols.length; j++)
+          //Wrap the row in quotes
+          //If the column contains an image, output the image pi
+          if (cols[j].querySelectorAll('img').length==1) {
+            var prev = cols[j].querySelectorAll('img')
+            row.push(prev[0].src.split("&",1))
+          } else {
+            row.push('"'+cols[j].innerText+'"');
+          }
+          csv.push(row.join(","));
+        };
+      
+        // Download CSV file
+        downloadCSV(csv.join("\n"), filename);
+      }
+      
+      function downloadCSV(csv, filename) {
+        var csvFile;
+        var downloadLink;
+      
+        // CSV file
+        csvFile = new Blob([csv], {type: "text/csv"});
+      
+        // Download link
+        downloadLink = document.createElement("a");
+      
+        // File name
+        downloadLink.download = filename;
+      
+        // Create a link to the file
+        downloadLink.href = window.URL.createObjectURL(csvFile);
+      
+        // Hide download link
+        downloadLink.style.display = "none";
+      
+        // Add the link to DOM
+        document.body.appendChild(downloadLink);
+      
+        // Click download link
+        downloadLink.click();
+      }
+      
+      // Credit: Chris Coyier codepen.io/chriscoyier/pen/tIuBL
+      (function(document) {
+        'use strict';
+      
+        var LightTableFilter = (function(Arr) {
+      
+          var _input;
+      
+          function _onInputEvent(e) {
+            _input = e.target;
+            var tables = document.getElementsByClassName(_input.getAttribute('data-table'));
+            Arr.forEach.call(tables, function(table) {
+              Arr.forEach.call(table.tBodies, function(tbody) {
+                Arr.forEach.call(tbody.rows, _filter);
+              });
+            });
+          }
+      
+          function _filter(row) {
+            var text = row.textContent.toLowerCase(), val = _input.value.toLowerCase();
+            row.style.display = text.indexOf(val) === -1 ? 'none' : 'table-row';
+          }
+      
+          return {
+            init: function() {
+              var inputs = document.getElementsByClassName('light-table-filter');
+              Arr.forEach.call(inputs, function(input) {
+                input.oninput = _onInputEvent;
+              });
+            }
+          };
+        })(Array.prototype);
+      
+        document.addEventListener('readystatechange', function() {
+          if (document.readyState === 'complete') {
+            LightTableFilter.init();
+          }
+        });
+      
+      })(document);
+
+      $(document).ready(function() {
+        $('#ochreTable').DataTable({
+          "pageLength": 25,
+          "lengthMenu": [ [10, 25, 50, 100, -1], [10, 25, 50, 100, "All"] ],
+          "deferRender": true,
+          language: {
+            search: "_INPUT_",
+            searchPlaceholder: "Search records..."
+          },
+          "columnDefs": [
+            { "width": "10%", "targets": 0 },
+            { "width": "20%", "targets": 1 },
+            { "width": "20%", "targets": 2 }
+          ]
+        });
+      } );
+      
+      
